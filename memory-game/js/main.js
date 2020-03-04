731e8f15e0e39ecab7de6ad535bc3b94f20a0448
Samuel Ling <samuel@192-168-1-100.tpgi.com.au>
2020-03-04 14:09:50 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,23 +1,19 @@
-console.log("Up and running!")
-
 let cards = ["queen", "queen", "king", "king"]
-
-
-
 let cardsInPlay = []
 
-let cardOne = cards[0]
-let cardTwo = cards[2]
-
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardTwo)
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  	alert("You found a match!");
-  } else 
-  	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	}	else {
+		console.log("Sorry, try again."); 
+	}
 }
+function flipCard(cardId) {
+	console.log("User flippd " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	if (cardsInPlay.length === 2) {
+  	checkForMatch()
+}}
 
-
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+flipCard(0)
+flipCard(2)
